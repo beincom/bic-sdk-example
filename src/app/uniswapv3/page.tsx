bd0e8b95a11eb21bc23de75d1648fcf25310d865
0xted97 <khiemsanh1997@gmail.com>
2024-07-24 11:32:35 +0700

    feat: sample form Swap

--- src/app/uniswapv3/page.tsx
+++ src/app/uniswapv3/page.tsx
@@ -7,11 +7,13 @@ import { BicSmartAccount } from "@/types";
 import { getSmartAccount } from "@/wallet";
 
 import LoginForm from "@/components/LoginForm";
+import { BIC_ADDRESS, FUSDT_ADDRESS } from "@/utils";
 
 const SwapTokenUniswap = () => {
   const [input1Value, setInput1Value] = useState("");
   const [input2Value, setInput2Value] = useState("");
-  const [selectedToken, setSelectedToken] = useState("");
+  const [selectedToken0, setSelectedToken0] = useState("");
+  const [selectedToken1, setSelectedToken1] = useState("");
   const [smartAccount, setSmartAccount] = useState<BicSmartAccount>();
 
   const [session] = useLocalStorage("session", {});
@@ -24,8 +26,12 @@ const SwapTokenUniswap = () => {
     setInput2Value(event.target.value);
   };
 
-  const handleTokenChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedToken(event.target.value);
+  const handleToken0Change = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedToken0(event.target.value);
+  };
+
+  const handleToken1Change = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedToken1(event.target.value);
   };
 
   const handleSwap = () => {
@@ -43,30 +49,63 @@ const SwapTokenUniswap = () => {
 
   return (
     <div className="bg-gray-200 p-4">
-      <LoginForm />
+      <div>
+        <LoginForm />
+      </div>
       <h1 className="text-2xl font-bold mb-4">Swap Token Uniswap</h1>
-      <input
-        type="text"
-        value={input1Value}
-        onChange={handleInput1Change}
-        className="border border-gray-300 rounded-md px-2 py-1 mb-2"
-      />
-      <input
-        type="text"
-        value={input2Value}
-        onChange={handleInput2Change}
-        className="border border-gray-300 rounded-md px-2 py-1 mb-2"
-      />
-      <select
-        value={selectedToken}
-        onChange={handleTokenChange}
-        className="border border-gray-300 rounded-md px-2 py-1 mb-2"
-      >
-        <option value="">Select Token</option>
-        <option value="Token1">Token 1</option>
-        <option value="Token2">Token 2</option>
-        <option value="Token3">Token 3</option>
-      </select>
+
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="email"
+        >
+          Sell
+        </label>
+        <div>
+          <select
+            value={selectedToken0}
+            onChange={handleToken0Change}
+            className="border border-gray-300 rounded-md px-2 py-1 mb-2"
+          >
+            <option value={FUSDT_ADDRESS}>FUST</option>
+            <option value={BIC_ADDRESS}>BIC</option>
+          </select>
+          <input
+            className="shadow appearance-none border rounded w-1/3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="amount0"
+            type="number"
+            onChange={handleInput1Change}
+            placeholder="Enter amount0"
+          />
+        </div>
+      </div>
+
+      <div className="mb-4">
+        <label
+          className="block text-gray-700 text-sm font-bold mb-2"
+          htmlFor="email"
+        >
+          Buy
+        </label>
+        <div>
+          <select
+            value={selectedToken1}
+            onChange={handleToken1Change}
+            className="border border-gray-300 rounded-md px-2 py-1 mb-2"
+          >
+            <option value={FUSDT_ADDRESS}>FUST</option>
+            <option value={BIC_ADDRESS}>BIC</option>
+          </select>
+          <input
+            className="shadow appearance-none border rounded w-1/3 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="amount1"
+            type="number"
+            onChange={handleInput2Change}
+            placeholder="Enter amount1"
+          />
+        </div>
+      </div>
+
       <button
         onClick={handleSwap}
         className="bg-blue-500 text-white px-4 py-2 rounded-md"
