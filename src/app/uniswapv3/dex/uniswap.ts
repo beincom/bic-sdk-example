d2c476927206413d2356d856a90309d61ee57d61
0xted97 <khiemsanh1997@gmail.com>
2024-07-30 15:10:39 +0700

    feat: test new version sdk

--- src/app/uniswapv3/dex/uniswap.ts
+++ src/app/uniswapv3/dex/uniswap.ts
@@ -1,4 +1,9 @@
-import { ETH_NATIVE_ADDRESS, ETH_WRAPPED_ADDRESS, UNISWAP_FACTORY_ADDRESS, UNISWAP_ROUTER_ADDRESS } from "@/utils";
+import {
+  ETH_NATIVE_ADDRESS,
+  ETH_WRAPPED_ADDRESS,
+  UNISWAP_FACTORY_ADDRESS,
+  UNISWAP_ROUTER_ADDRESS,
+} from "@/utils";
 import { UniswapAdapter, UniswapHelper } from "@beincom/dex";
 
 export const uniswapHelper = new UniswapHelper({
@@ -8,9 +13,9 @@ export const uniswapHelper = new UniswapHelper({
   multicall3Address: "0xcA11bde05977b3631167028862bE2a173976CA11",
   providerUrl:
     "https://arbitrum-sepolia.rpc.thirdweb.com/e1f8d427e28ebc5bb4e5ab5c38e8d665",
+  graphUrl:
+    "https://api.studio.thegraph.com/query/75955/my-uniswapv3-arbitrum-sepolia/0.0.1",
 });
-
-
 
 export const uniswapAdapter = new UniswapAdapter({
   factoryAddress: UNISWAP_FACTORY_ADDRESS,
