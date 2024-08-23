2d4b894f3a8f1c3ad75c2062f0ba47765a72200c
0xted97 <khiemsanh1997@gmail.com>
2024-08-23 18:40:25 +0700

    feat: example get history

--- src/utils/payments.ts
+++ src/utils/payments.ts
@@ -4,11 +4,11 @@ import { TOKEN_MESSAGE_EMITTER_ADDRESS } from "./constants";
 import { getSmartAccount } from "@/wallet";
 
 // Get env nhu DEX
-// export const paymentService = new PaymentService({
-//   tokenMessageEmitterAddress: TOKEN_MESSAGE_EMITTER_ADDRESS, 
-//   providerUrl:
-//     "https://arbitrum-sepolia.rpc.thirdweb.com/e1f8d427e28ebc5bb4e5ab5c38e8d665",
-// });
+export const paymentService = new PaymentService({
+  tokenMessageEmitterAddress: TOKEN_MESSAGE_EMITTER_ADDRESS, 
+  providerUrl:
+    "https://arbitrum-sepolia.rpc.thirdweb.com/e1f8d427e28ebc5bb4e5ab5c38e8d665",
+});
 
 
 // const smartAccount = await getSmartAccount();
